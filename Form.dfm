7f5ce4bf20f3cb0143d67d0fc2304daaaad406ad
Maulana0012 <waytrial03@gmail.com>
2024-06-09 00:59:16 +0800

    project updated from new form and unit created

--- Form.dfm
+++ Form.dfm
@@ -1,9 +1,9 @@
 object Form2: TForm2
-  Left = 192
-  Top = 144
+  Left = 196
+  Top = 169
   Width = 727
   Height = 450
-  Caption = 'Form2'
+  Caption = 'Login'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -13,20 +13,25 @@ object Form2: TForm2
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 304
+    Top = 160
+    Width = 3
+    Height = 13
+  end
   object Edit1: TEdit
     Left = 304
     Top = 128
     Width = 121
     Height = 21
     TabOrder = 0
-    Text = 'Edit1'
   end
   object btn1: TButton
     Left = 328
     Top = 176
     Width = 75
     Height = 25
-    Caption = 'btn1'
+    Caption = 'Login'
     TabOrder = 1
     OnClick = btn1Click
   end
--- Form.pas
+++ Form.pas
@@ -10,6 +10,7 @@ type
   TForm2 = class(TForm)
     Edit1: TEdit;
     btn1: TButton;
+    Label1: TLabel;
     procedure btn1Click(Sender: TObject);
   private
     { Private declarations }
@@ -29,26 +30,42 @@ uses Unit1;
 procedure TForm2.btn1Click(Sender: TObject);
 begin
 Form1.mm1.items[3].Visible := True;
-if Edit1.Text = 'admin' then
-begin
-  Form1.mm1.Items[1].Visible:= True;
-  Form1.mm1.Items[2].Visible:= False;
-  Form1.mm1.Items[3].Visible:= False;
-end else if Edit1.Text = 'kasir' then
-begin
-  Form1.mm1.Items[1].Visible:= False;
-  Form1.mm1.Items[2].Visible:= True;
-  Form1.mm1.Items[3].Visible:= False;
-end else if Edit1.Text = 'pemilik' then
-begin
-  Form1.mm1.Items[1].Visible:= False;
-  Form1.mm1.Items[2].Visible:= False;
-  Form1.mm1.Items[3].Visible:= True;
-end else
-begin
 
-end;;
-Form2.Close;
+if Edit1.Text = 'admin' then
+  begin
+    Form1.mm1.Items[1].Visible:= True;
+    Form1.mm1.Items[2].Visible:= False;
+    Form1.mm1.Items[3].Visible:= False;
+    Label1.Caption := '';
+    Edit1.Text := '';
+    Form2.Close;
+  end
+
+else if Edit1.Text = 'kasir' then
+  begin
+    Form1.mm1.Items[1].Visible:= False;
+    Form1.mm1.Items[2].Visible:= True;
+    Form1.mm1.Items[3].Visible:= False;
+    Label1.Caption := '';
+    Edit1.Text := '';
+    Form2.Close;
+  end
+
+else if Edit1.Text = 'pemilik' then
+  begin
+    Form1.mm1.Items[1].Visible:= False;
+    Form1.mm1.Items[2].Visible:= False;
+    Form1.mm1.Items[3].Visible:= True;
+    Label1.Caption := '';
+    Edit1.Text := '';
+    Form2.Close;
+  end
+
+else
+  begin
+    Label1.Caption := 'User does not exist';
+    Edit1.Text := '';
+  end
 end;
 
 end.
